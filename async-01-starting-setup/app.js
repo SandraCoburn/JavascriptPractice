dcfc91d9387652784b28fb5cd9406a8830168758
Sandra Coburn <sandra_coburn@hotmail.com>
2020-06-09 09:09:12 -0700

    Promises and chaining multiple Promies

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -1,17 +1,38 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition((success) => {
+      resolve(success);
+    }, opts);
+  });
+  return promise;
+};
+
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    (postData) => {
-      setTimeout(() => {
-        console.log(postData);
-      }, 2000);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+  let positionData;
+  getPosition()
+    .then((postData) => {
+      positionData = postData;
+      // console.log(postData)
+      return setTimer(2000);
+    })
+    .then((data) => {
+      console.log(data, positionData);
+    });
+  setTimer(1000).then(() => {
+    console.log("Timer done!");
+  });
   console.log("Clicked!... getting position...");
 }
 
