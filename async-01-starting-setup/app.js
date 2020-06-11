a56f0cf1143e20ff66e1122089c9bf40f8c35fbb
Sandra Coburn <sandra_coburn@hotmail.com>
2020-06-11 07:47:33 -0700

    Fixed typo and missing curly brace

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -23,32 +23,32 @@ async function trackUserHandler() {
   // let positionData;
   // await getPosition()
   let postData;
-  let timerDAta;
+  let timerData;
   try {
-     postData = await getPosition()
-     timerData = await setTimer(2000);
+    postData = await getPosition();
+    timerData = await setTimer(2000);
   } catch (error) {
-   console.log(error)
+    console.log(error);
   }
-   console.log(postData, timerData);
-    // .then(
-    //   (postData) => {
-    //     positionData = postData;
-    //     // console.log(postData)
-   
-      // },
-    //   (err) => {
-    //     console.log(err);
-    //   }
-    // ) //or you can use a .catch() to handle errors
-    // .then((data) => {
-   
-    // });
-//   setTimer(1000).then(() => {
-//     console.log("Timer done!");
-//   });
-//   console.log("Clicked!... getting position...");
-// }
+  console.log(postData, timerData);
+  // .then(
+  //   (postData) => {
+  //     positionData = postData;
+  //     // console.log(postData)
+
+  // },
+  //   (err) => {
+  //     console.log(err);
+  //   }
+  // ) //or you can use a .catch() to handle errors
+  // .then((data) => {
+
+  // });
+  //   setTimer(1000).then(() => {
+  //     console.log("Timer done!");
+  //   });
+  //   console.log("Clicked!... getting position...");
+}
 
 button.addEventListener("click", trackUserHandler);
 
